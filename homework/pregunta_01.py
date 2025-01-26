b9ffb889b81c0a5cae39b0bcd1bce8600b78297a
SQV <saquinterova@unal.edu.co>
2025-01-26 15:46:42 +0000

    Done

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -13,3 +13,60 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    import pandas as pd
+    import os
+
+    def preprocess_column(df, column_name, transformations):
+        for transformation in transformations:
+            df[column_name] = transformation(df[column_name])
+
+    def replace_characters(series, chars_to_replace, replacement):
+        for char in chars_to_replace:
+            series = series.str.replace(char, replacement)
+        return series
+
+
+    df = pd.read_csv('files/input/solicitudes_de_credito.csv', sep=';', index_col=0)
+    df.dropna(inplace=True)
+    
+    preprocess_column(df, 'sexo', [lambda x: x.str.upper()])
+
+    preprocess_column(df, 'tipo_de_emprendimiento', [lambda x: x.str.upper(), lambda x: x.str.strip()])
+
+    preprocess_column(df, 'barrio', [
+        lambda x: x.str.upper(), 
+        lambda x: replace_characters(x, ["_", "-"], " ")
+    ])
+
+    preprocess_column(df, 'idea_negocio', [
+        lambda x: x.str.upper(), 
+        lambda x: replace_characters(x, ["_", "-"], " "), 
+        lambda x: x.str.strip()
+    ])
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.strip().str.replace("$", "").str.replace(",", "").str.replace(".00", "").astype(int)
+
+    preprocess_column(df, 'línea_credito', [
+        lambda x: x.str.upper(), 
+        lambda x: replace_characters(x, ["_", "-"], " "), 
+        lambda x: x.str.strip()
+    ])
+
+    df['fecha_homologada'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True, errors='coerce')
+    df['fecha_homologada'] = df['fecha_homologada'].fillna(
+        pd.to_datetime(df['fecha_de_beneficio'], format="%Y/%m/%d", errors='coerce')
+    )
+    
+    df.drop(columns='fecha_de_beneficio', inplace=True)
+    df.rename(columns={'fecha_homologada': 'fecha_de_beneficio'}, inplace=True)
+
+    df.drop_duplicates(inplace=True)
+
+    output_directory = os.path.join('files', 'output')
+    os.makedirs(output_directory, exist_ok=True)
+    df.to_csv(os.path.join(output_directory, 'solicitudes_de_credito.csv'), sep=';')
+
+
+    return df
+
+print(pregunta_01())
